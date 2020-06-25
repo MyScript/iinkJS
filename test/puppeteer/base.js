582ddfd350dfd900857689bd7e75db65bad6e279
steven.roulleau <steven.roulleau@myscript.com>
2020-06-25 17:20:45 +0200

    chore(test): update mocha to v8, enable parallel + rollup v2.18

--- test/puppeteer/base.js
+++ test/puppeteer/base.js
@@ -1,20 +1,20 @@
 import puppeteer from 'puppeteer'
-import { after, before } from 'mocha'
 
 const globalVar = {
   browser: {}
 }
 
-before(async () => {
-  global.browser = await puppeteer.launch({
-    executablePath: 'google-chrome-stable',
-    args: ['--disable-dev-shm-usage', '--no-sandbox', '--disable-setuid-sandbox'],
-    headless: true
-  })
-})
+exports.mochaHooks = {
+  async beforeAll () {
+    global.browser = await puppeteer.launch({
+      executablePath: 'google-chrome-stable',
+      args: ['--disable-dev-shm-usage', '--no-sandbox', '--disable-setuid-sandbox'],
+      headless: true
+    })
+  },
+  afterAll () {
+    browser.close()
 
-after(() => {
-  browser.close()
-
-  global.browser = globalVar.browser
-})
+    global.browser = globalVar.browser
+  }
+}
